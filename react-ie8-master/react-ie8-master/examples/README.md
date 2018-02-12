4ebbaa727afd7d600caa4b88f1b10069b1a7a6c9
wangyanfang <yaobo324413@163.com>
2018-02-12 14:00:26 +0800

    2 commit

---
+++ react-ie8-master/react-ie8-master/examples/README.md
@@ -0,0 +1,13 @@
+# React IE8 examples
+
+A series of examples which works in IE8.
+
+## Getting start
+
+```shell
+$ cd <an-example-folder>
+$ npm install
+$ npm start
+```
+
+Open your browser at http://localhost:8080
---
+++ react-ie8-master/react-ie8-master/examples/object-assign/entry.js
@@ -0,0 +1,34 @@
+/**
+ * CANNOT use `import` to import `es5-shim`,
+ * because `import` will be transformed to `Object.defineProperty` by babel,
+ * `Object.defineProperty` doesn't exists in IE8,
+ * (but will be polyfilled after `require('es5-shim')` executed).
+ */
+
+require('es5-shim');
+require('es5-shim/es5-sham');
+require('console-polyfill');
+require('core-js/fn/object/assign');
+
+/**
+ * CANNOT use `import` to import `react` or `react-dom`,
+ * because `import` will run `react` before `require('react-ie8')()`.
+ */
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const oldData = {
+  hello: 'World',
+};
+
+const newData = Object.assign({}, oldData, {
+  hello: 'New World',
+});
+
+ReactDOM.render(
+  <h1>{newData.hello}</h1>,
+  document.getElementById('app')
+);
